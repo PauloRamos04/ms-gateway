1fa865a0c4c465cef2b8a384a7695ba0de6b5f9f
Paulo <pauloramos.04@hotmail.com>
2024-09-21 14:21:50 -0300

    sim

---
+++ src/main/java/saudeconectada/fatec/gateway/MessageProducer.java
@@ -0,0 +1,16 @@
+package saudeconectada.fatec.gateway;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageProducer {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(String message) {
+        rabbitTemplate.convertAndSend("myQueue", message);
+    }
+}
---
+++ src/main/java/saudeconectada/fatec/gateway/RabbitConfig.java
@@ -0,0 +1,14 @@
+package saudeconectada.fatec.gateway;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public Queue myQueue() {
+        return new Queue("myQueue", false);
+    }
+}
